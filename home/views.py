3b000dcd0e5fe3c4759b0451b569f0ccf0fc71bf
Carlos <carlos@Cristianes-MacBook-Pro.local>
2020-08-30 13:22:40 -0300

    First commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import logout
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def my_logout(request):
+    logout(request)
+    return redirect('home')
